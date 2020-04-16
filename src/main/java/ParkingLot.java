a518424b9b4afc34abc1f769597419c46f17e89e
rahulgidde <rahulgidde1212@gmail.com>
2020-04-16 10:48:58 +0530

    Added:Test Case For Check Vehice Already Parked Or Not

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -1,7 +1,18 @@
 public class ParkingLot {
-
-    public boolean getParking(Object o) {
+    Object vehicle = null;
+    public boolean park(Object vehicle) {
+        if(this.vehicle != null)
+        return false;
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if(this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
         
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,11 +1,28 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+
+    ParkingLot parkingLot = null;
+    Object vehicle = null;
+
+    @Before
+    public void set(){
+     parkingLot = new ParkingLot();
+        vehicle = new Object();
+    }
+
     @Test
-    public void givenVehicle_WhenPark_ShouldReturnTrue() {
-        ParkingLot parkingLot = new ParkingLot();
-        boolean isParked = parkingLot.getParking(new Object());
+    public void givenVehicle_WhenParked_ShouldReturnTrue() {
+        boolean isParked = parkingLot.park(new Object());
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+        parkingLot.park(vehicle);
+        boolean isParked = parkingLot.park(vehicle);
+        Assert.assertFalse(isParked);
     }
 }
